1fce82d68f92a656010d961167670931fdd80c28
Ivan MSI <arigatory@gmail.com>
2022-06-17 20:36:01 +0300

    adding 1st test

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,11 @@
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+
+        }
+    }
+}
