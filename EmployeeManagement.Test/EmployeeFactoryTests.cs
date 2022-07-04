a64f54a0884c64876d650a13e754edbe805a1c2e
Ivan MSI <arigatory@gmail.com>
2022-07-04 23:09:18 +0300

    first test

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -1,11 +1,19 @@
-namespace EmployeeManagement.Test
-{
-    public class EmployeeFactoryTests
-    {
-        [Fact]
-        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
-        {
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
 
-        }
+namespace EmployeeManagement.Test;
+
+public class EmployeeFactoryTests
+{
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+    {
+        var employeeFactory = new EmployeeFactory();
+
+        var employee = (InternalEmployee) employeeFactory.CreateEmployee("Ivan", "Panchenko");
+
+        Assert.Equal(2500, employee.Salary);
+
+
     }
 }
